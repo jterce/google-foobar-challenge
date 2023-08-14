506e6192adb32e348b111e496ad29744a3c0cfe7
jterce <jterceno001@gmail.com>
2023-08-14 07:52:54 +0200

    ready for level 3 (3/3)

---
+++ Level3/fuel-injection-perfection/solution.py
@@ -0,0 +1,14 @@
+def solution(n):
+
+    return n
+
+
+if __name__ == "__main__":
+    sol1 = solution('15')
+    print(sol1, sol1 == 5)
+
+    sol2 = solution('4')
+    print(sol2, sol2 == 2)
+
+    # sol3 = solution('9'*310)
+    # print(sol3, sol3 == 2)
